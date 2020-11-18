2820cf4cae7c4466fa263a264f85d3aad91c7563
amassana <amassana@it.is>
2020-11-18 12:48:31 +0100

    Settings configurables

--- src/main/java/com/sg/scheduler/task/ScheduledCurrentTimeTask.java
+++ src/main/java/com/sg/scheduler/task/ScheduledCurrentTimeTask.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +14,18 @@ public class ScheduledCurrentTimeTask {
 
   private static final Logger log = LoggerFactory.getLogger(ScheduledCurrentTimeTask.class);
 
+  @Value("${application.sleepTime}")
+  private long sleepTime;
+
   private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
   @Scheduled(fixedRate = 5000)
-  @SchedulerLock(name = "currentTimeTask", lockAtLeastFor = "5S", lockAtMostFor = "1M")
+  @SchedulerLock(name = "currentTimeTask",
+          lockAtLeastFor = "${application.schedulerLock.lockAtLeastFor}",
+          lockAtMostFor = "${application.schedulerLock.lockAtMostFor}")
   public void currentTimeTask() throws InterruptedException {
     log.info("The time is now {} START", dateFormat.format(new Date()));
-    Thread.sleep(10000);
+    Thread.sleep(sleepTime);
     log.info("The time is now {} END", dateFormat.format(new Date()));
   }
 
